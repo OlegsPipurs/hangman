292963ec71e410d434093f29f07bff9b995f1bc0
Olegs <esen4el2@inbox.lv>
2021-11-01 20:24:44 +0200

    8. Vārdu grūtības pakāpes noteikšana III

--- hangman.py
+++ hangman.py
@@ -3,18 +3,20 @@ from scripts_test.split_difficulity import *
 
 import random
 while (visi_vardi):                                           #cikls kurā izspēlē vārdus
-    speles_limenis = input('Izvēlies spēles līmeni, 1 = easy,  2 = medium,  3 = latvietis =], 4 = iziet!!! :')         #Spele izvada pieejamos limenus
-    if speles_limenis == '1':                                                                                          #Parbaude vai speletajs izvelejies pirmo limeni
+    difficulty = input('Izvēlies spēles līmeni, 1 = easy,  2 = medium,  3 = latvietis =], 4 = iziet!!! :')         #Spele izvada pieejamos limenus
+    if difficulty == '1':                                                                                          #Parbaude vai speletajs izvelejies pirmo limeni
         print('Tu izvēlējies pirmo spēles limeni "easy"')
         random.shuffle(importetais_vards_easy)
-        game.play(importetais_vards_easy_lv_fix.pop())                                                                 #
-    elif speles_limenis == '2':                                                                                        #Parbaude vai speletajs izvelejies otro limeni
-        print('Tu izvēlējies otro spēles limeni "medium"')                                                             #
-        game.play(importetais_vards_medium_lv_fix.pop())                                                               #
-    elif speles_limenis == '3':                                                                                        #Parbaude vai speletajs izvelejies Treso limeni
-        print('Tu izvēlējies trešo spēles limeni "latvietis =]"')                                                      #
+        game.play(importetais_vards_easy_lv_fix.pop())                                                             #
+    elif difficulty == '2':                                                                                        #Parbaude vai speletajs izvelejies otro limeni
+        print('Tu izvēlējies otro spēles limeni "medium"')  
+        random.shuffle(importetais_vards_medium)                                                                   #
+        game.play(importetais_vards_medium_lv_fix.pop())                                                           #
+    elif difficulty == '3':                                                                                        #Parbaude vai speletajs izvelejies Treso limeni
+        print('Tu izvēlējies trešo spēles limeni "latvietis =]"')  
+        random.shuffle(importetais_vards_hard)                                                                     #
         game.play(importetais_vards_hard_lv_fix.pop())
-    elif speles_limenis == '4':                                                                                        #Parbaude vai speletajs izvelejies iziet no speles
+    elif difficulty == '4':                                                                                        #Parbaude vai speletajs izvelejies iziet no speles
         print ("Tiksimies nākamreiz!")
         break 
     else:
