aa35263a05ffe69a243efc69eb9d5c0d8249155b
Olegs <esen4el2@inbox.lv>
2021-11-06 15:09:30 +0200

    Speles vizualizacija

--- gameplay/game.py
+++ gameplay/game.py
@@ -7,30 +7,33 @@ class game:
         neuzminetie_burti = set()
         neuzminetie_vardi = set()
         dzivibas = 6
-        win = 0          # Speles karodzina definejums izmantots 1 un 0    
+        win = 0          # Speles karodzina definejums izmantots 1 un 0
+        print(chr(27) + "[2J")    
         print(73*'='+'\nSpēle karātavas. Minamais vārds ir '+str(len(minamais_vards))+' simboli garš. Tev ir sešas dzīvības.\n'+73*'=') # Izvada speles pazinojumu ar ,ima,o vardu garumu + Speles vizualizacija atdalijums ar "=" 
 
         while dzivibas != 0:                                                    # While cikla sakums definējums dzivibas nav= ar 0
             burts = input('Ievadi minēto burtu: ').upper()                      # Ievaditais burts tiek parveidots uz lielo burtu
+            print(chr(27) + "[2J") 
             if burts.isalpha() == False:                                        # Parbauda vai simbols ir alfabetisks
-                print('Ievadīji nekorektu simbolu')                             # Izvada pazinojumu, nekorektu simbolu ievadi
+                # https://ozzmaker.com/add-colour-to-text-in-python/
+                print('\033[0;31;40m Ievadīji nekorektu simbolu' + '\033[0;37;40m')                     # Izvada pazinojumu, nekorektu simbolu ievadi
             elif burts in minamais_vards and len(burts) == 1:                   # Tiek veikts parbaudijums uz ievadito burtu, vai ir alfabetisks un garums ir vienads ar 1
                 uzmietie_burti.add(burts)                                       # Pievieno sarakstam uzminamie burti
                 neuzminetie_burti.add(burts)                                    # Pievieno sarakstam neminamie burti
                 print(neuzminetie_burti)                                        # Izvada neuzminetos burtus
-                print('Atminētie burti: ', ''.join(burts if burts in uzmietie_burti else '-' for burts in minamais_vards))    # Veic parbaudi par uzmineto burtu minamaja varda, ja tas nav uzminets atstaj "-" 
                 if ''.join(burts if burts in uzmietie_burti else '-' for burts in minamais_vards) == minamais_vards:          # Pārbauda vai vārds ir pa burtiem atminēts tb vai atmaskotais vārds sakrīt ar minamo vārdu
-                    win = 1                                                                                                   # Speles gaita mainas karodzina vērtiba
+                    win = 1
+                    print('\033[0;32;40m Tu atminēji vārdu! Vārds bija: ',minamais_vards  + '\033[0;37;40m')         # Izdrukā paziņojumu par vārda atminēšanu                                                                                                   # Speles gaita mainas karodzina vērtiba
                     break
             elif len(burts) > 1 and len(burts) != len(minamais_vards):           # Tiek veikta parbaude ievadita simbolu garums ir vienads ar minamo varda garumu    
-                if burts == 'IDDQD':                                             # easter egg čīterime atminoties klasiku!!! =] doom ftw \m/
-                    print('  '.join(minamais_vards[::2].upper()))                 #Izvada katru otro burtu !BONUS
+                if burts == 'fita':                                              # easter egg čīterime atminoties klasiku!!! =] doom ftw \m/
+                    print('  '.join(minamais_vards[::2].upper()))                # Izvada katru otro burtu !BONUS
                 else:
-                    print('Nepareizs vārda garums, mini vēlreiz')
+                    print('\033[0;31;40m Nepareizs vārda garums, mini vēlreiz' + '\033[0;37;40m')
             elif len(burts) > 1 and len(burts) == len(minamais_vards) and burts != minamais_vards:  #Tiek veikta parbaude minamais vards tika uzminets
                 neuzminetie_vardi.add(burts)
                 print(neuzminetie_vardi)
-                print('Nepareizs vārds, mini vēlreiz!') 
+                print('\033[0;31;40m Nepareizs vārds, mini vēlreiz!' + '\033[0;37;40m') 
                 dzivibas -= 1                                                    # Nepareiza minejuma gaidjuma atņem dzivibu
                 print('Atlikušās dzīvības: ' + dzivibas * "\u2764\uFE0F")        # Izvada atlikuso dzivu skaitu
             elif burts == minamais_vards:                                        #Vards tika uzminets pa burtiem, tiek mainits karodzina veriba uz Win "True"
@@ -41,10 +44,74 @@ class game:
             else:
                 neuzminetie_burti.add(burts)                                    # Papildina spēlētāja ievadīto burtu neuzminēto burtu sarakstā
                 print(neuzminetie_burti)                                        # Izdrukā neuzminēto burtu sarakstu
-                print('Minētais', burts, 'nav šajā vārdā')                      # Izdrukā paziņojumu, ka spēlētāja ievadītāis burts nav minamajā vārdā
+                print('\033[0;31;40m Minētais', burts, 'nav šajā vārdā' + '\033[0;37;40m')                      # Izdrukā paziņojumu, ka spēlētāja ievadītāis burts nav minamajā vārdā
                 dzivibas -= 1                                                   # Atņem vienu dzīvību par nepareizu atbildi
                 print('Atlikušās dzīvības: ' + dzivibas * "\u2764\uFE0F")       # Izdrukā paziņojumu ar atlikušo dzīvību skaitu
-        if win == 1:                                                            # Ja spēlēs karodziņa vērtība ir "True"
-                print('Tu atminēji vārdu! Vārds bija: ',minamais_vards)         # Izdrukā paziņojumu par vārda atminēšanu
-        else:
-                print('Tu neatminēji vārdu. Vārds bija: ',minamais_vards)       # Izdrukā paziņojumu par vārda neuzminēšanu
+            
+            print('Atminētie burti: ', ''.join(burts if burts in uzmietie_burti else '-' for burts in minamais_vards) + "\033[0;37;40m") # Veic parbaudi par uzmineto burtu minamaja varda, ja tas nav uzminets atstaj "-" 
+            
+            if dzivibas == 6: 
+                print('        |========  ')
+                print('        ||      |  ') 
+                print('        ||         ') 
+                print('        ||         ')
+                print('        ||         ')
+                print('        ||         ')
+                print('___†††======†††____')
+            else:
+                if dzivibas == 5: 
+                    print('        |========  ')
+                    print('        ||      |  ') 
+                    print('        ||      o   ') 
+                    print('        ||         ')
+                    print('        ||         ')
+                    print('        ||         ')
+                    print('___†††======†††____')
+                else:
+                    if dzivibas == 4: 
+                        print('        |========  ')
+                        print('        ||      |  ') 
+                        print('        ||      o  ') 
+                        print('        ||      |  ')
+                        print('        ||         ')
+                        print('        ||         ')
+                        print('___†††======†††____')
+                    else:
+                        if dzivibas == 3: 
+                            print('        |========  ')
+                            print('        ||      |  ') 
+                            print('        ||      o  ') 
+                            print('        ||     -|  ')
+                            print('        ||         ')
+                            print('        ||         ')
+                            print('___†††======†††____')
+                        else:
+                            if dzivibas == 2: 
+                                print('        |========  ')
+                                print('        ||      |  ') 
+                                print('        ||      o  ') 
+                                print('        ||     -|- ')
+                                print('        ||         ')
+                                print('        ||         ')
+                                print('___†††======†††____')
+                            else:
+                                if dzivibas == 1: 
+                                    print('        |========  ')
+                                    print('        ||      |  ') 
+                                    print('        ||      o  ') 
+                                    print('        ||     -|- ')
+                                    print('        ||     /   ')
+                                    print('        ||         ')
+                                    print('___†††======†††____')
+                                else:
+                                    if win == 1:                                                            # Ja spēlēs karodziņa vērtība ir "True"
+                                            print('\033[0;32;40m Tu atminēji vārdu! Vārds bija: ',minamais_vards + "\033[0;37;40m")         # Izdrukā paziņojumu par vārda atminēšanu
+                                    else:
+                                        print('        |========  ')
+                                        print('        ||      |  ') 
+                                        print('        ||      o  ') 
+                                        print('        ||     -|- ')
+                                        print('        ||     / \ ')
+                                        print('        ||         ')
+                                        print('___†††======†††____')
+                                        print('\033[0;31;40m Tu neatminēji vārdu. Vārds bija: ',minamais_vards + "\033[0;37;40m")       # Izdrukā paziņojumu par vārda neuzminēšanu
--- hangman.py
+++ hangman.py
@@ -1,23 +1,50 @@
-from gameplay.game import game  
+from gameplay.game import game
 from scripts_test.split_difficulity import *
 
 import random
-while (visi_vardi):                                           #cikls kurā izspēlē vārdus
-    difficulty = input('Izvēlies spēles līmeni, 1 = easy,  2 = medium,  3 = latvietis =], 4 = iziet!!! :')         #Spele izvada pieejamos limenus
-    if difficulty == '1':                                                                                          #Parbaude vai speletajs izvelejies pirmo limeni
+import time
+import shelve
+
+print('\033[0;32;40m'''' ____  __   _____ __________    _________________________   _________    _________''')
+print('''|    |/ _| /  _  \\\______   \  /  _  \__    ___/  _  \   \ /   /  _  \  /   _____/''')
+print('''|      <  /  /_\  \|       _/ /  /_\  \|    | /  /_\  \   Y   /  /_\  \ \_____  \ ''')
+print("""|    |  \/    |    \    |   \/    |    \    |/    |    \     /    |    \/        \\""")
+print("""|____|__ \____|__  /____|_  /\____|__  /____|\____|__  /\___/\____|__  /_______  / \033[0;37;40m""")
+print('\n')
+nick = input('IEVADI SAVU IESAUKU:')
+while (visi_vardi):                                                                                                 #cikls kurā izspēlē vārdus
+    difficulty = input('Izvēlies spēles līmeni, 1 = easy,  2 = medium,  3 = latvietis =], 4 = highscore, 5 = iziet!!!:')         #Spele izvada pieejamos limenus
+    start_time = time.time()
+    if difficulty == '1':
+        difftxt = 'easy'                                                                                          #Parbaude vai speletajs izvelejies pirmo limeni
         print('Tu izvēlējies pirmo spēles limeni "easy"')
         random.shuffle(importetais_vards_easy)
         game.play(importetais_vards_easy_lv_fix.pop())                                                             #
-    elif difficulty == '2':                                                                                        #Parbaude vai speletajs izvelejies otro limeni
+    elif difficulty == '2':
+        difftxt = 'medium'                                                                                        #Parbaude vai speletajs izvelejies otro limeni
         print('Tu izvēlējies otro spēles limeni "medium"')  
         random.shuffle(importetais_vards_medium)                                                                   #
         game.play(importetais_vards_medium_lv_fix.pop())                                                           #
-    elif difficulty == '3':                                                                                        #Parbaude vai speletajs izvelejies Treso limeni
+    elif difficulty == '3':
+        difftxt = 'latvietis'                                                                                        #Parbaude vai speletajs izvelejies Treso limeni
         print('Tu izvēlējies trešo spēles limeni "latvietis =]"')  
         random.shuffle(importetais_vards_hard)                                                                     #
         game.play(importetais_vards_hard_lv_fix.pop())
-    elif difficulty == '4':                                                                                        #Parbaude vai speletajs izvelejies iziet no speles
+    elif difficulty == '4':                                                                                       #Parbaude vai speletajs izvelejies iziet no speles
+        print ("HIGHSCORES")
+        print('===========')
+        with open('data\score.txt', 'r', encoding='utf-8') as file_read:
+            contents = file_read.read()
+            print(contents)  
+            break
+    elif difficulty == '5':                                                                                       #Parbaude vai speletajs izvelejies iziet no speles
         print ("Tiksimies nākamreiz!")
         break 
     else:
         print('Izvēlies spēles piedāvātos līmeņus')
+    end_time = time.time()
+    speles_laiks = int(end_time - start_time)
+    x = str(speles_laiks)
+    print("Spele beigusies, laiks ir" , speles_laiks , 'sekundes')
+    with open('data\score.txt', 'a') as file:
+        file.write(nick + ' | ' + "Speles laiks " + (str(speles_laiks)) + ' | ' + "Speles limenis " + difftxt + '\n')
